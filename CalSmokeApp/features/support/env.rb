c3110b702e42fa0a363cadb5a5fed1b0ee695c65
James Treanor <james@intercom.io>
2017-01-11 11:52:59 +0000

    Benchmark query

--- CalSmokeApp/features/support/env.rb
+++ CalSmokeApp/features/support/env.rb
@@ -5,6 +5,7 @@ World(Calabash::Cucumber::Operations)
 
 require 'rspec'
 require 'chronic'
+require 'benchmark'
 
 # Pry is not allowed on the Xamarin Test Cloud.  This will force a validation
 # error if you mistakenly submit a binding.pry to the Test Cloud.
@@ -12,4 +13,20 @@ if !ENV['XAMARIN_TEST_CLOUD']
   require 'pry'
   Pry.config.history.file = '.pry-history'
   require 'pry-nav'
+end
+
+module Calabash
+  module Cucumber
+    module Core
+      alias_method :old_query, :query
+      def query(uiquery, *args)
+        result = nil
+        time = Benchmark.realtime do
+          result = old_query(uiquery, *args)
+        end
+        calabash_info "query '#{uiquery}' took #{time*1000} milliseconds"
+        result
+      end
+    end
+  end
 end
